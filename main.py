2dd005f12cb28d6e52330f6d16d9fc8698d9de6e
Noreen Wu <noreenwu@gmail.com>
2020-01-04 17:23:48 -0500

    minor change to see if pipeline activates

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Healthy!")
 
 
 @APP.route('/auth', methods=['POST'])
